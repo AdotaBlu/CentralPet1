42d8d0b1341366a67863091d7b588957c6e99ad2
lorxr <lorenco.zambiazi@gmail.com>
2023-08-01 22:52:10 -0300

    Mapeamento da classe Adocao

--- src/main/java/centralpet/modelo/entidade/adocao/Adocao.java
+++ src/main/java/centralpet/modelo/entidade/adocao/Adocao.java
@@ -3,28 +3,65 @@ package centralpet.modelo.entidade.adocao;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.Table;
+
 import centralpet.modelo.entidade.ong.Ong;
 import centralpet.modelo.entidade.pet.Pet;
 import centralpet.modelo.entidade.termo.Termo;
 import centralpet.modelo.entidade.tutor.Tutor;
 import centralpet.modelo.enumeracao.adocao.StatusAdocao;
 
+@Entity
+@Table(name = "adocao")
 public class Adocao implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_adocao")
 	private Long id;
 	
+	@Column(name = "pet_adocao", nullable = false, unique = false)
+	//1 pet pode ter apenas uma adoção e 1 adoção apenas 1 pet?
+	@OneToOne(fetch = FetchType.LAZY)
+	@MapsId
+	@JoinColumn(name = "id_pet")
 	private Pet pet;
 	
+	@Column(name = "ong_adocao", nullable = false, unique = false)
+	@OneToMany(fetch = FetchType.LAZY)
+	@MapsId
+	@JoinColumn(name = "id_ong")
 	private Ong ong;
 	
+	@Column(name = "tutor_adocao", nullable = false, unique = false)
+	@OneToMany(fetch = FetchType.LAZY)
+	@MapsId
+	@JoinColumn(name = "id_tutor")
 	private Tutor tutor;
 	
+	@Column(name = "termo_adocao", nullable = false, unique = false)
+	@OneToMany(fetch = FetchType.LAZY)
+	@MapsId
+	@JoinColumn(name = "id_termo")
 	private Termo termo;
 	
+	@Column(name = "status_adocao", nullable = false, unique = false)
 	private StatusAdocao statusAdocao;
 	
+	@Column(name = "data_adocao", nullable = false, unique = false)
 	private LocalDate dataAdocao;
 	
 	public Adocao () {}
