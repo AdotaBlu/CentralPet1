db0209d061ea3d8a6179365248b203ee775e11a7
Erik <122393715+ErikUller04@users.noreply.github.com>
2023-08-03 10:56:09 -0300

    Implementada a interface DAO Termo

---
+++ src/main/java/centralpet/modelo/dao/termo/TermoDAO.java
@@ -0,0 +1,20 @@
+package centralpet.modelo.dao.termo;
+
+import java.util.List;
+
+import centralpet.modelo.entidade.termo.Termo;
+
+public interface TermoDAO {
+
+	void inserirTermo(Termo termo);
+	
+	void deletarTermo(Termo termo);
+	
+	void atualizarTermo(Termo termo);
+	
+	List<Termo> recuperarTermo(Termo id);
+	
+	List<Termo> recuperarTermosOng(Termo ong);
+	
+	List<Termo> recuperarTodosTermos();
+}
