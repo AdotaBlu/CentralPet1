6e0eb2c5c96aebaff1b2f302f3c6e16c0aa5a4cb
Erik Uller <erikuller.profissional@gmail.com>
2023-08-18 11:16:44 -0300

    Teste no main

--- src/main/java/centralpet/Principal.java
+++ src/main/java/centralpet/Principal.java
@@ -1,17 +1,21 @@
 package centralpet;
 
-import centralpet.modelo.dao.contato.ContatoDAO;
-import centralpet.modelo.dao.contato.ContatoDAOImpl;
+import java.time.LocalDate;
+
 import centralpet.modelo.dao.endereco.EnderecoDAO;
 import centralpet.modelo.dao.endereco.EnderecoDAOImpl;
-import centralpet.modelo.entidade.contato.Contato;
+import centralpet.modelo.dao.tutor.TutorDAO;
+import centralpet.modelo.dao.tutor.TutorDAOImpl;
 import centralpet.modelo.entidade.endereco.Endereco;
+import centralpet.modelo.entidade.tutor.Tutor;
+import centralpet.modelo.enumeracao.genero.GeneroTutor;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
 		EnderecoDAO enderecoDAO = new EnderecoDAOImpl();
+		TutorDAO tutorDAO = new TutorDAOImpl();
 		
 		String logradouro = "rua emilio tallmann";
 		short numero = 1755;
@@ -26,6 +30,21 @@ public class Principal {
 		endereco.setCep(cep);
 		
 		enderecoDAO.inserirEndereco(endereco);
+		
+		String cpf = "12345678911";
+		LocalDate dataNascimento = LocalDate.of(2004, 04, 02);
+		GeneroTutor generoTutor = GeneroTutor.MASCULINO;
+		String nome = "Jao";
+		
+		Tutor tutor = new Tutor();
+		
+		tutor.setNome(nome);
+		tutor.setEndereco(endereco);
+		tutor.setCpf(cpf);
+		tutor.setDataNascimento(dataNascimento);
+		tutor.setGeneroTutor(generoTutor);
+		
+		tutorDAO.inserirTutor(tutor);
 		
 		
 	}
--- src/main/java/centralpet/modelo/dao/usuario/UsuarioDAO.java
+++ src/main/java/centralpet/modelo/dao/usuario/UsuarioDAO.java
@@ -2,7 +2,6 @@ package centralpet.modelo.dao.usuario;
 
 import java.util.List;
 
-import centralpet.modelo.entidade.contato.Contato;
 import centralpet.modelo.entidade.usuario.Usuario;
 
 public interface UsuarioDAO {
